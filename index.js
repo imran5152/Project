e3e541df7e71ca54b03fb90fc2bfd81dff429e46
imran5152 <afridkhan9371@gmail.com>
2025-03-19 17:04:43 +0530

    git commit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let express = require('express')
 let app = express()
 let fs = require('fs')
+app.use(express.json());
 
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+
+app.get('/greet/:name', (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello, ${name}!`);
+});
 
 
 app.listen(2300,()=> console.log('Server started'))
